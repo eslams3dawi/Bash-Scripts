ccdaee0586a0a9433c48682c2da1ac7f80bf0c8c
eslams3dawi <eslamseadawy717@gmail.com>
2026-02-27 17:41:37 +0200

    Add FileExtensionConverter Script

---
+++ FileExtensionConverter.sh
@@ -0,0 +1,27 @@
+dir_path="$1"
+fromExtension="$2"
+toExtension="$3"
+
+if [[ ! -d "$dir_path" ]]; then
+        echo "Error: Directory not found."
+        echo "Usage: ./"$S0" directory_path originalExtension wantedExtension"
+        exit 1;
+fi
+
+echo "Directory found!"
+echo "Searching for files with extension \"$fromExtension\"..."
+
+converted=false
+
+for file in "$dir_path"/*"$fromExtension"; do
+        if [[ -f "$file" ]]; then
+                new_file="${file%$fromExtension}$toExtension"
+                mv "$file" "$new_file"
+                echo "File $file is successfully converted to $new_file."
+                converted=true
+        fi
+done
+
+if [[ ! "$converted" ]]; then
+        echo "No files with extension \"$fromExtension\""
+fi
